fcf65059f99f8fcabb53fdcb1167576ebd924bbe
João Victor Sales Santos <cadavsales@gmail.com>
2023-11-10 18:46:59 -0600

    started table data

--- src/components/configForm.jsx
+++ src/components/configForm.jsx
@@ -1,8 +1,23 @@
 import styles from './configForm.module.css';
 import { supabase } from '../client';
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 export function ConfigForm() {
+
+  const [fetchError, setFetchError] = useState(null)
+  const [partners, setPartners] = useState(null)
+
+  
+  async function getData() {
+    let { data: partnersData, error } = await supabase
+    .from('partners')
+    .select('*')
+    console.log(JSON.stringify(partnersData));
+  }
+
+  getData()
+      
 
     const navigate = useNavigate();
 
@@ -13,7 +28,11 @@ export function ConfigForm() {
     }
 
     return (
+
         <div>
+            <div className = {styles.table}>
+              <h2> Hello Magda, I am still constructing this part of the website, be patient 🐸 </h2>
+            </div>
             <button onClick={logOut}> Log out </button>
         </div>
     )
